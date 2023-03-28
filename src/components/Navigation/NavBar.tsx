11cdd7d4e17c5cdf8a3a266247ca1af8782d5d31
macha <kazuki.aishima.dev@gmail.com>
2023-03-28 23:53:46 +0900

    03/28

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -13,45 +13,41 @@ const MENU_LIST = [
 ];
 
 export default function NavBar() {
-  const [navActive, setNavActive] = useState(false);
+  const [navActive, setNavActive] = useState(true);
   const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
-    <header className=" sticky z-30 top-0">
-      <nav className="flex p-16 justify-between items-center bg-green-500">
-        <Link href={"/"}>
-          <div>
-            <h1 className=" text-3xl ">マちゃー</h1>
-          </div>
-        </Link>
-        <div
-          onClick={() => setNavActive(!navActive)}
-          className={" flex flex-col gap-6 cursor-pointer "}
-        >
-          <div className=" w-40 h-4 bg-black rounded-sm "></div>
-          <div className=" w-40 h-4 bg-black rounded-sm "></div>
-          <div className=" w-40 h-4 bg-black rounded-sm "></div>
-          <div className=" w-40 h-4 bg-black rounded-sm "></div>
-          <div className=" w-40 h-4 bg-black rounded-sm "></div>
-        </div>
-        <div
-          className={
-            '${navActive ? "active" : ""} flex flex-col fixed top-60 w-72 px-6 py-4 transition-all min-h-screen bg-green-800 active:right-0'
-          }
-        >
-          {MENU_LIST.map((menu, idx) => (
-            <div
-              onClick={() => {
-                setActiveIdx(idx);
-                setNavActive(false);
-              }}
-              key={menu.text}
-            >
-              <NavItem active={activeIdx === idx} {...menu} />
+    <div className=" m-0 p-0 outline-none box-border">
+      <header className=" sticky z-30 top-0">
+        <nav className="flex p-4 justify-between items-center bg-green-500">
+          <Link href={"/"}>
+            <div className=" decoration-inherit">
+              <h1 className=" text-3xl ">{String(navActive)}</h1>
             </div>
-          ))}
-        </div>
-      </nav>
-    </header>
+          </Link>
+          <div
+            onClick={() => setNavActive(!navActive)}
+            className=" flex flex-col gap-y-1.5 cursor-pointer "
+          >
+            <div className=" w-8 h-0.5 bg-black rounded-sm "></div>
+            <div className=" w-8 h-0.5 bg-black rounded-sm "></div>
+            <div className=" w-8 h-0.5 bg-black rounded-sm "></div>
+          </div>
+          <div className="flex flex-col fixed top-16 w-72 gap-y-6 -right-72 pr-6 pl-4 transition-all duration-200 min-h-[100vh - 60px] bg-green-800 active:right-0">
+            {MENU_LIST.map((menu, idx) => (
+              <div
+                onClick={() => {
+                  setActiveIdx(idx);
+                  setNavActive(true);
+                }}
+                key={menu.text}
+              >
+                <NavItem active={activeIdx === idx} {...menu} />
+              </div>
+            ))}
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 }
--- src/components/Navigation/NavItem.tsx
+++ src/components/Navigation/NavItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const NavItem = ({ text, href, active }: any) => {
   return (
     <Link href={href}>
-      <div className={'nav__item ${active ? "active" : ""} text-inherit'}>{text}</div>
+      <div className=" text-inherit ">{text}</div>
     </Link>
   );
 };
