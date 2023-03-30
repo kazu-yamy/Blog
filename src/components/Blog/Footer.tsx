e941ef0ea1f3879a51cd663218e63d34a43248c3
macha <kazuki.aishima.dev@gmail.com>
2023-03-30 21:05:54 +0900

    03/30

---
+++ src/components/Blog/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className=" bg-gray-100 ">
+      <div className=" max-w-4xl w-full mx-auto h-24 flex items-center justify-center">
+        <div>My Blog</div>
+      </div>
+    </footer>
+  );
+};
