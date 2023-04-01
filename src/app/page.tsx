be97e423b398b6264daa371a8aca73dae3e92323
macha <kazuki.aishima.dev@gmail.com>
2023-04-01 23:53:52 +0900

    04/01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <h1 className=' text-3xl font-bold'>
-      Hello World !!
     </h1>
   )
 }
--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -13,29 +13,27 @@ const MENU_LIST = [
 ];
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
   const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
     <nav className=" flex items-center justify-between flex-wrap bg-teal-500 p-6 ">
       <Link href={"/"}>
         <div className=" flex items-center flex-shrink-0 text-white mr-6">
-          <h1 className=" font-semibold text-xl tracking-tight">Sate Of MACCHA
+          <h1 className=" font-semibold text-xl tracking-tight">
+            Sate Of MACCHA
           </h1>
         </div>
       </Link>
       <div className="block lg:hidden">
         <button className=" flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <div className=" fill-current h-3 w-3 "></div>
-          <div className=" fill-current h-3 w-3 "></div>
-          <div className=" fill-current h-3 w-3 "></div>
+          <div className=" fill-current h-3 w-9 font-semibold ">Menu</div>
         </button>
       </div>
-      <div
-        className=" w-full block flex-grow lg:flex lg:items-center lg:w-auto"
-      >
+      <div className=" hidden w-full md:block flex-grow lg:flex lg:items-center lg:w-auto lg:text-center ">
         {MENU_LIST.map((menu, idx) => (
           <div
-          className="text-sm lg:flex-grow"
+            className="text-sm lg:flex-grow"
             onClick={() => {
               setActiveIdx(idx);
             }}
