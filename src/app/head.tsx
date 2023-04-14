2da1a4cdabdcfb9852aea8aed4bc0c227ec44341
kazu-yamy <kazuki.aishima.dev@gmail.com>
2023-04-14 16:56:34 +0900

    04/14

--- src/app/head.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/no-unescaped-entities */
-export default function Head() {
-  return (
-    <>
-      <title>MACCHA's Blog</title>
-    </>
-  );
-}
--- src/components/Blog/Footer.tsx
+++ src/components/Blog/Footer.tsx
@@ -1,9 +1,7 @@
 export function Footer() {
   return (
-    <div className=" bg-gray-100 ">
-      <div className=" w-full mx-auto h-24 flex items-center justify-center absolute bottom-0 ">
+      <div className=" bg-gray-100 w-full mx-auto h-24 flex items-center justify-center absolute bottom-0 ">
         My Blog
       </div>
-    </div>
   );
 }
